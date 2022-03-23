4777aa3259c59a69dee65146d87fee087b8819e3
Maxim Bokarev <maxim.bokarev@gmail.com>
2022-03-23 16:51:19 +0200

    ModelAttribute

--- src/main/java/ua/bokarev/springcourse/controllers/PeopleController.java
+++ src/main/java/ua/bokarev/springcourse/controllers/PeopleController.java
@@ -3,16 +3,15 @@ package ua.bokarev.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ua.bokarev.springcourse.dao.PersonDAO;
+import ua.bokarev.springcourse.model.Person;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-    private PersonDAO personDAO;
+    private final PersonDAO personDAO;
 
     @Autowired
     public PeopleController(PersonDAO personDAO) {
@@ -20,15 +19,25 @@ public class PeopleController {
     }
 
     @GetMapping()
-    public String index(Model model){
-    //Получаем всех людей из DAO и передаем на отображение в представление
+    public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
+
     @GetMapping("/{id}")
-    public String show(@PathVariable("id") int id, Model model){
-        //Получим одного человека по его id из DAO и передадим на отображение в представление
+    public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
--- src/main/java/ua/bokarev/springcourse/dao/PersonDAO.java
+++ src/main/java/ua/bokarev/springcourse/dao/PersonDAO.java
@@ -10,18 +10,26 @@ import java.util.List;
 public class PersonDAO {
     private static int PEOPLE_COUNT;
     private List<Person> people;
+
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Tom"));
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
         people.add(new Person(++PEOPLE_COUNT, "Bob"));
         people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT,"Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
     }
 
     public List<Person> index() {
         return people;
     }
-    public Person show(int id){
-        return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
