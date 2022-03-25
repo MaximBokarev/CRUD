ad58744bc9a920605936a8aaac2bccf3924738d6
Maxim Bokarev <maxim.bokarev@gmail.com>
2022-03-25 08:11:01 +0200

    JDBC Template

---
+++ src/main/java/ua/bokarev/springcourse/dao/PersonMapper.java
@@ -0,0 +1,22 @@
+package ua.bokarev.springcourse.dao;
+
+
+import org.springframework.jdbc.core.RowMapper;
+import ua.bokarev.springcourse.model.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+
+    @Override
+    public Person mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Person person = new Person();
+
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+        return person;
+    }
+}
